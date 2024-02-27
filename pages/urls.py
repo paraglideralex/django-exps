d9e7409cdd4dda9b63c4e33c4d683004ef2b35e5
Александр Тюшин <avtyushin@gosniias.ru>
2024-02-27 11:40:36 +0300

    добавил вывод графика

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 # pages/urls.py
 from django.urls import path
  
-from .views import HomePageView, calculate_sum
+from .views import HomePageView, calculate_sum, plot_graph
  
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('calculate_sum/', calculate_sum, name='calculate_sum'),
+    path('plot/', plot_graph, name='plot_graph'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,6 +5,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 from django.views.generic import TemplateView
 import re
+from django.http import HttpResponse
+import matplotlib.pyplot as plt
+import numpy as np
+from io import BytesIO
  
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -14,19 +18,26 @@ def calculate_sum(request):
     if request.method == 'POST':
         value1 = request.POST.get('value1', 0)
         value2 = request.POST.get('value2', 0)
-        vv = check(value1)
+        # vv = check(value1)
         result = sum(value1, value2)
         return JsonResponse({'result': result})
     else:
         return JsonResponse({'error': 'Метод не поддерживается'}, status=405)
 
-def check(value):
-    match = re.match("^(0|[1-9]\d*)([.,]\d+)?", value)
-    groups = match.groups()
-    if (len(groups)==2):
-        return f'{groups[0]}{groups[1]}'
-    else:
-        return groups[0]
-
 def sum(value1, value2):
-    return value1 + value2
+    return float(value1) + float(value2)
+
+def plot_graph(request):
+    x = np.arange(0, 10, 1)
+    y = 2 * x
+
+    plt.plot(x, y)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('График y=2*x')
+
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+
+    return HttpResponse(buffer, content_type='image/png')
